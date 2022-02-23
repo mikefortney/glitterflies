757a0b29b573103fc64f1bd6965b79c490baa13f
Mike Fortney <mike@inspired.com>
2022-02-22 21:38:59 -0800

    temp remove auto scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { MutableRefObject, useEffect } from 'react';
+import { MutableRefObject, useEffect, useRef } from 'react';
 import Content from 'components/Content';
 import Hero from 'components/Hero';
-import useHeroScroll from 'hooks/useHeroScroll';
+// import useHeroScroll from 'hooks/useHeroScroll';
 import GlobalStyle from 'globalStyles';
 import smoothscroll from 'smoothscroll-polyfill';
 
 const App: React.FC = () => {
-  const { contentRef } = useHeroScroll();
+  // const { contentRef } = useHeroScroll();
+  const contentRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     smoothscroll.polyfill();
   }, []);
